2147a50e35b23c548bff9c9758a065d8bff3da68
Олег Солдатов <oleg.soldatov@live.ru>
2018-07-10 00:13:44 +0300

    Модель продукции перенесена в один файл. Для поля ImageId создан шаблон редактора.

--- Azox/Areas/Admin/Controllers/ProductsController.vb
+++ Azox/Areas/Admin/Controllers/ProductsController.vb
@@ -5,10 +5,12 @@ Imports Soldata.Imaging
 Namespace Areas.Admin.Controllers
 	<Authorize>
 	Public Class ProductsController
-		Inherits DbController
+		Inherits Controller
+
+		Private ReadOnly Property ProductManager As New ProductManager
 
 		Async Function Index(searchString As String, Optional pageIndex As Integer = 0, Optional pageSize As Integer = 20) As Task(Of ActionResult)
-			Dim entities = Db.Products.AsQueryable
+			Dim entities = ProductManager.Entities
 
 			' Поиск.
 			If Not String.IsNullOrEmpty(searchString) Then
@@ -29,8 +31,7 @@ Namespace Areas.Admin.Controllers
 		<HttpPost, ValidateAntiForgeryToken>
 		Async Function Create(model As Product) As Task(Of ActionResult)
 			If ModelState.IsValid Then
-				Db.Products.Add(model)
-				Await Db.SaveChangesAsync
+				Await ProductManager.CreateAsync(model)
 				TempData("Message") = "Продукт добавлен."
 				Return RedirectToAction("Edit", New With {model.Id})
 			End If
@@ -41,7 +42,7 @@ Namespace Areas.Admin.Controllers
 			If IsNothing(id) Then
 				Return New HttpStatusCodeResult(HttpStatusCode.BadRequest)
 			End If
-			Dim model = Await Db.Products.FindAsync(id)
+			Dim model = Await ProductManager.FindByIdAsync(id)
 			If IsNothing(model) Then
 				Return HttpNotFound()
 			End If
@@ -49,10 +50,9 @@ Namespace Areas.Admin.Controllers
 		End Function
 
 		<HttpPost, ValidateAntiForgeryToken>
-		Async Function Edit(model As Product, returnUrl As String) As Task(Of ActionResult)
+		Async Function Edit(model As Product) As Task(Of ActionResult)
 			If ModelState.IsValid Then
-				Db.Entry(model).State = EntityState.Modified
-				Await Db.SaveChangesAsync
+				Await ProductManager.UpdateAsync(model)
 				TempData("Message") = "Продукт изменен."
 				Return RedirectToAction("Edit", New With {model.Id})
 			End If
@@ -63,7 +63,7 @@ Namespace Areas.Admin.Controllers
 			If IsNothing(id) Then
 				Return New HttpStatusCodeResult(HttpStatusCode.BadRequest)
 			End If
-			Dim model = Await Db.Products.FindAsync(id)
+			Dim model = Await ProductManager.FindByIdAsync(id)
 			If IsNothing(model) Then
 				Return HttpNotFound()
 			End If
@@ -71,78 +71,72 @@ Namespace Areas.Admin.Controllers
 		End Function
 
 		<ActionName("Delete"), HttpPost, ValidateAntiForgeryToken>
-		Public Async Function DeleteConfirmed(id As Guid, returnUrl As String) As Task(Of ActionResult)
-			Dim entity = Await Db.Products.FindAsync(id)
-			If Not IsNothing(entity.ImageId) Then
-				Db.Images.Remove(Await Db.Images.FindAsync(entity.ImageId))
-			End If
-			Db.Products.Remove(entity)
-			Await Db.SaveChangesAsync
+		Async Function DeleteConfirmed(id As Guid) As Task(Of ActionResult)
+			Await ProductManager.DeleteAsync(Await ProductManager.FindByIdAsync(id))
+			'Dim entity = Await Db.Products.FindAsync(id)
+			'If Not IsNothing(entity.ImageId) Then
+			'	Db.Images.Remove(Await Db.Images.FindAsync(entity.ImageId))
+			'End If
+			'Db.Products.Remove(entity)
+			'Await Db.SaveChangesAsync
 			TempData("Message") = "Продукт удален."
 			Return RedirectToAction("Index")
 		End Function
 
-		Public Function ChangeImage(id As Guid) As ActionResult
-			Return View(New ChangeImageViewModel With {.Id = id})
+		Function ChangeImage(id As Guid) As ActionResult
+			ViewBag.Id = id
+			Return View()
 		End Function
 
-		<HttpPost>
-		<ValidateAntiForgeryToken>
-		Public Async Function ChangeImage(model As ChangeImageViewModel) As Task(Of ActionResult)
+		<HttpPost, ValidateAntiForgeryToken>
+		Async Function ChangeImage(model As ChangeImageViewModel) As Task(Of ActionResult)
 			If Not IsNothing(model.ImageFile) AndAlso model.ImageFile.ContentType.Contains("image") Then
-				Dim entity = Await Db.Products.FindAsync(model.Id)
+				'Dim entity = Await Db.Products.FindAsync(model.Id)
 
-				If Not IsNothing(entity.ImageId) Then
-					Db.Images.Remove(Await Db.Images.FindAsync(entity.ImageId))
-				End If
+				'If Not IsNothing(entity.ImageId) Then
+				'	Db.Images.Remove(Await Db.Images.FindAsync(entity.ImageId))
+				'End If
 
-				Dim image As New Image With {
-						.Original = ImageUtility.FileToBytes(model.ImageFile.InputStream, model.ImageFile.ContentType),
-						.Thumbnail = ImageUtility.FileToBytes(model.ImageFile.InputStream, model.ImageFile.ContentType, 200, 200, StretchMode.UniformToFill),
-						.Large = ImageUtility.FileToBytes(model.ImageFile.InputStream, model.ImageFile.ContentType, 555, 555, StretchMode.UniformToFill),
-						.Medium = ImageUtility.FileToBytes(model.ImageFile.InputStream, model.ImageFile.ContentType, 260, 260, StretchMode.UniformToFill),
-						.Small = ImageUtility.FileToBytes(model.ImageFile.InputStream, model.ImageFile.ContentType, 160, 160, StretchMode.Uniform, Drawing.Color.White)
-					}
+				'Dim image As New Image With {
+				'		.Original = ImageUtility.FileToBytes(model.ImageFile.InputStream, model.ImageFile.ContentType),
+				'		.Thumbnail = ImageUtility.FileToBytes(model.ImageFile.InputStream, model.ImageFile.ContentType, 200, 200, StretchMode.UniformToFill),
+				'		.Large = ImageUtility.FileToBytes(model.ImageFile.InputStream, model.ImageFile.ContentType, 555, 555, StretchMode.UniformToFill),
+				'		.Medium = ImageUtility.FileToBytes(model.ImageFile.InputStream, model.ImageFile.ContentType, 260, 260, StretchMode.UniformToFill),
+				'		.Small = ImageUtility.FileToBytes(model.ImageFile.InputStream, model.ImageFile.ContentType, 160, 160, StretchMode.Uniform, Drawing.Color.White)
+				'	}
 
-				Db.Images.Add(image)
-				entity.ImageId = image.Id
-				Await Db.SaveChangesAsync
+				'Db.Images.Add(image)
+				'entity.ImageId = image.Id
+				'Await Db.SaveChangesAsync
 
 				TempData("Message") = "Изображение изменено."
 			End If
 
-			Return RedirectToAction("edit", New With {model.Id})
+			Return RedirectToAction("Edit", New With {model.Id})
 		End Function
 
 		Public Async Function DeleteImage(id As Guid) As Task(Of ActionResult)
-			Dim entity = Await Db.Products.FindAsync(id)
+			'Dim entity = Await Db.Products.FindAsync(id)
 
-			If Not IsNothing(entity.ImageId) Then
-				Db.Images.Remove(Await Db.Images.FindAsync(entity.ImageId))
-			End If
+			'If Not IsNothing(entity.ImageId) Then
+			'	Db.Images.Remove(Await Db.Images.FindAsync(entity.ImageId))
+			'End If
 
-			entity.ImageId = Nothing
-			Await Db.SaveChangesAsync
-			TempData("Message") = "Изображение удалено."
+			'entity.ImageId = Nothing
+			'Await Db.SaveChangesAsync
+			'TempData("Message") = "Изображение удалено."
 
-			Return RedirectToAction("edit", New With {id})
+			Return RedirectToAction("Edit", New With {id})
 		End Function
 
-		'''' <summary>
-		'''' Проверяет ярлык сущности на совпадение. Если ярлык совпадает с существующим, то ему добавляется числовой суффикс.
-		'''' </summary>
-		'''' <param name="slug">Ярлык.</param>
-		'''' <param name="id">Идентификатор сущности.</param>
-		'''' <param name="suffix">Числовой суффикс. По умолчанию 0.</param>
-		'''' <returns>Строка, содержащая ярлык.</returns>
-		'Protected Friend Async Function ValidateSlugAsync(slug As String, id As Guid, Optional suffix As Integer = 0) As Task(Of String)
-		'	Dim result = If(suffix = 0, slug, String.Join("-", slug, suffix))
-
-		'	If Await Db.Products.AnyAsync(Function(m) m.Slug.Equals(result) And Not m.Id.Equals(id)) Then
-		'		Return Await ValidateSlugAsync(slug, id, suffix + 1)
-		'	End If
-
-		'	Return result
-		'End Function
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing Then
+				If _ProductManager IsNot Nothing Then
+					_ProductManager.Dispose()
+					_ProductManager = Nothing
+				End If
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
 	End Class
 End Namespace
---
+++ Azox/Views/Shared/EditorTemplates/Image.vbhtml
@@ -0,0 +1,21 @@
+﻿@ModelType Guid?
+@If IsNothing(Model) Then
+	@Html.ActionLink("Добавить", "ChangeImage", New With {.Id = ViewBag.Id}, New With {.class = "btn btn-default btn-block"})
+Else
+	@Html.Hidden("", Model)
+	@<div class="row">
+		<div class="col-md-12">
+			<div class="thumbnail pull-left">
+				<a href="@Url.Action("ChangeImage", New With {.Id = ViewBag.Id})">
+					<img src="@Url.Action("Thumbnail", "Images", New With {.area = "", .id = Model})" alt="Изображение" />
+				</a>
+				<div class="text-right">
+					<a href="@Url.Action("DeleteImage", New With {.id = ViewBag.Id})" class="btn btn-link btn-sm" title="Удалить">
+						<span class="fa fa-remove"></span>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+End If
+
