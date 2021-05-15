4506e7bdc63b3b21e5c875c8de9de3ff5df5254b
Олег Солдатов <oleg.soldatov@live.ru>
2021-05-15 23:20:35 +0300

    Добавлена поддержка абсолютного пути в сущности.

---
+++ Soldata.Azox.Core/IPathable.vb
@@ -0,0 +1,11 @@
+﻿''' <summary>
+''' Предоставляет поддержку абсолютного пути.
+''' </summary>
+Public Interface IPathable
+	Inherits IEntity
+
+	''' <summary>
+	''' Абсолютный путь.
+	''' </summary>
+	Property AbsolutePath As String
+End Interface
