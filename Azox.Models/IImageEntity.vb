a4c9746eee4bc47fb99637a9d6bb5fa969bf8c24
Олег Солдатов <oleg.soldatov@live.ru>
2021-06-12 15:53:25 +0300

    Из решения удален более неподдерживаемый проект Azox.Models.

--- Azox.Models/IImageEntity.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Interface IImageEntity(Of TImage As {Class, IImage(Of Guid)})
-	Inherits IImageEntity(Of TImage, Guid, Guid)
-	Property ImageId As Guid?
-End Interface
-
-Public Interface IImageEntity(Of TImage As {Class, IImage(Of TImageKey)}, TImageKey, Out TKey)
-	Inherits IEntity(Of TKey)
-	Property Image As TImage
-End Interface
