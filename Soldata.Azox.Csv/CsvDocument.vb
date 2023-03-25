dff56359a35d2e987e6f96280fe792f1d8f8580c
Олег Солдатов <oleg.soldatov@live.ru>
2023-03-25 13:08:09 +0300

    Добавлен проект CSV-документа.

---
+++ Soldata.Azox.Csv/CsvDocument.vb
@@ -0,0 +1,51 @@
+﻿Imports System.IO
+Imports System.Text
+
+''' <summary>
+''' CSV-документ.
+''' </summary>
+Public Class CsvDocument
+    ''' <summary>
+    ''' Заголовки колонок.
+    ''' </summary>
+    Public Property ColumnHeaders As IList(Of String)
+
+    ''' <summary>
+    ''' Строки.
+    ''' </summary>
+    Public Property Rows As IList(Of String())
+
+    Public Sub New()
+        ColumnHeaders = New List(Of String)
+        Rows = New List(Of String())
+    End Sub
+
+    Public Shared Async Function ParseAsync(stream As Stream, encoding As Encoding, Optional separator As String = ";") As Task(Of CsvDocument)
+        Dim document As New CsvDocument
+
+        Using reader As New StreamReader(stream, encoding)
+            ' Получим заголовок, являющийся первой строкой в документе.
+            document.ColumnHeaders = (Await reader.ReadLineAsync).Split(separator).ToList
+
+            ' Получим строки.
+            Dim columnCount = document.ColumnHeaders.Count
+            Do Until reader.EndOfStream
+                Dim line = reader.ReadLine.Split(separator).ToList
+                Dim diff = line.Count - columnCount
+
+                ' Если в линии меньше ячеек, чем столбцов, то добавим пустые; Если в линии больше ячеек, чем столбцов, то удалим лишние.
+                If diff < 0 Then
+                    Do Until line.Count = columnCount
+                        line.Add(String.Empty)
+                    Loop
+                ElseIf diff > 0 Then
+                    line.RemoveRange(columnCount, diff)
+                End If
+
+                document.Rows.Add(line.ToArray)
+            Loop
+        End Using
+
+        Return document
+    End Function
+End Class
---
+++ Soldata.Azox.Csv/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Общие сведения об этой сборке предоставляются следующим набором
+' набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+' связанные с этой сборкой.
+
+' Проверьте значения атрибутов сборки
+
+<Assembly: AssemblyTitle("Soldata.Azox.Csv")>
+<Assembly: AssemblyDescription("CSV-документ.")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Azox")>
+<Assembly: AssemblyCopyright("© Софт Бизнес, 2023")>
+<Assembly: AssemblyTrademark("Azox")>
+
+<Assembly: ComVisible(False)>
+
+'Указанный ниже идентификатор GUID предназначен для идентификации библиотеки типов, если этот проект будет видимым для COM-объектов
+<Assembly: Guid("fe7b0a05-12d0-40ff-8a00-094a735259a6")>
+
+' Сведения о версии сборки состоят из указанных ниже четырех значений:
+'
+'      Основной номер версии
+'      Дополнительный номер версии
+'      Номер сборки
+'      Редакция
+'
+' Можно задать все значения или принять номера сборки и редакции по умолчанию 
+' используя "*", как показано ниже:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
